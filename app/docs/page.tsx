df6b248d59d5586baef7898dc226cc80e24bf4b5
Viraj Bhartiya <vlbhartiya@gmail.com>
2025-03-30 23:16:58 +0530

    feat: add redirect to documentation page for improved user navigation

---
+++ app/docs/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export default function DocsPage() {
+  redirect(
+    "https://buttered-kitten-dd7.notion.site/Parity-Protocol-19b4cdefc57181febd53ece6a654fc41"
+  );
+}
